76eab5153bab14c30bf422b424d032fad237ce4b
wangdan <dan.wang1@newtouch.com>
2026-01-22 12:15:01 +0800

    メッセージの変更

--- library_form/library_form/DeleteBookControl.cs
+++ library_form/library_form/DeleteBookControl.cs
@@ -86,7 +86,7 @@ namespace library_form
                 }
                 else if (i == remineder.Count - 1)
                 {
-                    MessageBox.Show("此书不存在！");
+                    MessageBox.Show("この本は存在しません");
                     break;
                 }
             }
