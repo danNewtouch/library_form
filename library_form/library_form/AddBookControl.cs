86eb31095608733426cb7cafb314955fafaadd44
danNewtouch <dan.wang1@newtouch.com>
2026-02-12 15:11:29 +0800

    Update AddBookControl.cs
    wo gai wang1

--- library_form/library_form/AddBookControl.cs
+++ library_form/library_form/AddBookControl.cs
@@ -37,7 +37,7 @@ namespace library_form
                 Writter.Clear();
                 PublishCompany.Clear();
                 Page.Clear();
-                MessageBox.Show("書籍の追加に成功しました。こちらはlocal");
+                MessageBox.Show("書籍の追加に成功しました。こちらはlocal wang1");
             }
 
         }
